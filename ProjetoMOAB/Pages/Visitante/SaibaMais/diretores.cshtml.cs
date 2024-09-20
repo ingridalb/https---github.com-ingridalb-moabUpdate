71c0f460498dd2f46f95fe3e430f37584b444e43
caue <cauesouzadelima7@gmail.com>
2024-09-20 15:51:29 -0300

    Descrição das alterações

---
+++ ProjetoMOAB/Pages/Visitante/SaibaMais/diretores.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoMOAB.Pages.Visitante.SaibaMais
+{
+    public class diretoresModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ProjetoMOAB/Pages/Visitante/SaibaMais/faleconosco.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoMOAB.Pages.Visitante.SaibaMais
+{
+    public class faleconoscoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
